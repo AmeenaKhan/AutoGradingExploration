088c5ef7bc1758da3331703b4f0ea550375e329b
AmeenaKhan <amekha2000@gmail.com>
2017-04-20 18:58:38 -0500

    altered to work with output of split_by_set.py

--- dict_to_csv.py
+++ dict_to_csv.py
@@ -1,23 +1,25 @@
 import csv
 
 def dict_to_csv(feature_dict):
-	with open('features.csv','w') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        #init_row = ["essay_id", "org_score", "pos_score", "complex_score"]
-        writer.writerow(["essay_id","score","org_score", "pos_adjadv" , "pos_noun", "pos_pronoun","pos_verb","pos_other", "pos_nums", "complex_short","complex_medium","complex_long"])
-       	for key in feature_dict:
-            row = []
-			row.append(key)
-			row.append(feature_dict[key]["score"])
-            row.append(feature_dict[key]["organization"])
-            row.append(feature_dict[key]["pos_adjadv"])
-            row.append(feature_dict[key]["pos_noun"])
-            row.append(feature_dict[key]["pos_pronoun"])
-            row.append(feature_dict[key]["pos_verb"])
-            row.append(feature_dict[key]["pos_other"])
-            row.append(feature_dict[key]["pos_nums"])
-            row.append(feature_dict[key]["complex_short"])
-            row.append(feature_dict[key]["complex_medium"])
-            row.append(feature_dict[key]["complex_long"])
-            writer.writerow(row)
-	csvfile.close()
+    for k in feature_dict:
+        with open('features.csv','w') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(["essay_id", "score", "org_score", "pos_adjadv", "pos_noun", "pos_pronoun", "pos_verb", "pos_other", "pos_nums", "complex_short", "complex_medium", "complex_long", "vocab_level"])
+            for key in feature_dict[k]:
+                row = []
+                row.append(key)
+                row.append(k)
+                row.append(feature_dict[k][key]["score"])
+                row.append(feature_dict[k][key]["organization"])
+                row.append(feature_dict[k][key]["pos_adjadv"])
+                row.append(feature_dict[k][key]["pos_noun"])
+                row.append(feature_dict[k][key]["pos_pronoun"])
+                row.append(feature_dict[k][key]["pos_verb"])
+                row.append(feature_dict[k][key]["pos_other"])
+                row.append(feature_dict[k][key]["pos_nums"])
+                row.append(feature_dict[k][key]["complex_short"])
+                row.append(feature_dict[k][key]["complex_medium"])
+                row.append(feature_dict[k][key]["complex_long"])
+                row.append(feature_dict[k][key]["vocab_level"])
+                writer.writerow(row)
+        csvfile.close()
